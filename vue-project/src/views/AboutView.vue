7086eece481fd84001f210061d768420481c9192
ImyourClone <158525345+ImyourClone@users.noreply.github.com>
2024-03-26 10:32:06 +0800

    first commit

---
+++ vue-project/src/views/AboutView.vue
@@ -0,0 +1,19 @@
+<template>
+  <div class="about">
+    <h1>Welcome to Note Todo</h1>
+  </div>
+    
+</template>
+
+<style>
+@media (min-width: 1024px) {
+  .about {
+    min-height: 100vh;
+    display: flex;
+    justify-items: center;
+  }
+  .h1{
+    color: aqua;
+  }
+  }
+</style>
